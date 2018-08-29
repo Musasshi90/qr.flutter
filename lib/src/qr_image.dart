a7fdf5ffff6744fd065978a28b045da238ddb5be
Musasshi90 <laupong90@gmail.com>
2018-08-29 21:56:52 +0800

    add setting in qr image
    add setting in qr image

--- lib/src/qr_image.dart
+++ lib/src/qr_image.dart
@@ -3,31 +3,46 @@
  * Copyright (c) 2018 the QR.Flutter authors.
  * See LICENSE for distribution and usage details.
  */
-import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:qr/qr.dart';
+import 'dart:ui' as sky;
+import 'package:flutter/material.dart';
 
 import 'qr_painter.dart';
 
 class QrImage extends StatelessWidget {
   QrImage({
-    @required String data,
+    this.data,
     this.size,
+    this.image,
     this.padding = const EdgeInsets.all(10.0),
     this.backgroundColor,
-    Color foregroundColor = const Color(0xFF000000),
-    int version = 4,
-    int errorCorrectionLevel = QrErrorCorrectLevel.L,
+    this.foregroundColor = const Color(0xFF000000),
+    this.version = 4,
+    this.errorCorrectionLevel = QrErrorCorrectLevel.L,
     this.onError,
-  }) : _painter = new QrPainter(
-            data, foregroundColor, version, errorCorrectionLevel,
-            onError: onError);
+    this.logoSize,
+    this.boxFit,
+  }) {
+    painter = new QrPainter(
+        data, foregroundColor, version, this.image, errorCorrectionLevel,
+        onError: onError,
+        boxFit: this.boxFit,
+        logoSize: logoSize);
+  }
 
-  final QrPainter _painter;
+  QrPainter painter;
   final Color backgroundColor;
   final EdgeInsets padding;
   final double size;
   final QrError onError;
+  final sky.Image image;
+  final String data;
+  final foregroundColor;
+  final int errorCorrectionLevel;
+  final version;
+  final double logoSize;
+  final BoxFit boxFit;
 
   @override
   Widget build(BuildContext context) {
@@ -39,9 +54,9 @@ class QrImage extends StatelessWidget {
           height: widgetSize,
           color: backgroundColor,
           child: new Padding(
-            padding: this.padding,
+            padding: padding,
             child: new CustomPaint(
-              painter: _painter,
+              painter: painter,
             ),
           ),
         );
